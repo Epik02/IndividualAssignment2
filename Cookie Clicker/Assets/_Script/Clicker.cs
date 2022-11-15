c5855e7c9ce6a4e5e5bcf2feb394f9556a27aa3a
Nicholas <chutzpahniko@gmail.com>
2022-11-15 16:30:44 -0500

    Final V1

--- Cookie Clicker/Assets/_Script/Clicker.cs
+++ Cookie Clicker/Assets/_Script/Clicker.cs
@@ -9,15 +9,21 @@ public class Clicker : MonoBehaviour
     public int cookies = 0;
     public int manualClickUpgrade = 1;
     public TMP_Text holder;
+    public TMP_Text upgradeCost;
+    public TMP_Text currentUpgrade;
 
     // Update is called once per frame
     void Update()
     {
+        upgradeCost.text = (manualClickUpgrade * 100).ToString();
+        currentUpgrade.text = manualClickUpgrade.ToString();
+
+
         if (Input.GetKeyDown("space"))
         {
             cookies = cookies + (1 * manualClickUpgrade);
             holder.text = cookies.ToString();
-            this.transform.localScale = new Vector3(1f, 1f, 1f);
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
         else
         {
